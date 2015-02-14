47ea64829ea6285dab428eb42819b69f1e36ebbc
Ryan Chen <chenjunhua1997@gmail.com>
2015-02-14 17:23:23 -0600

    ActivityMain update

--- app/src/main/java/com/example/ryan/cards/MainActivity.java
+++ app/src/main/java/com/example/ryan/cards/MainActivity.java
@@ -9,16 +9,23 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.*;
+
+import java.util.*;
 
 
-public class MainActivity extends ActionBarActivity {
-
+public class MainActivity extends ActionBarActivity
+{
+    Button card01,card02,card03,card04,card05,card06,card07,card08,card09,card10,card11,card12,card13,card14,card15,card16,card17,card18,card19,card20,card21,card22,card23,card24,card25,card26;
+    Button scard01,scard02,scard03,scard04,scard05,scard06,scard07,scard08,scard09,scard10,scard11,scard12,scard13,scard14,scard15,scard16,scard17,scard18,scard19,scard20,scard21,scard22,scard23,scard24,scard25,scard26;
+    Button rcard01,rcard02,rcard03,rcard04,rcard05,rcard06,rcard07,rcard08,rcard09,rcard10,rcard11,rcard12,rcard13,rcard14,rcard15,rcard16,rcard17,rcard18,rcard19,rcard20,rcard21,rcard22,rcard23,rcard24,rcard25,rcard26;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
+        if (savedInstanceState == null)
+        {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
@@ -27,21 +34,24 @@ public class MainActivity extends ActionBarActivity {
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
@@ -51,9 +61,11 @@ public class MainActivity extends ActionBarActivity {
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment
+    {
 
-        public PlaceholderFragment() {
+        public PlaceholderFragment()
+        {
         }
 
         @Override
